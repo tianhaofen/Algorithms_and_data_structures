d2240d62ecae86cf9e86315aa00fc58d267b84e0
tianhaofen <843576169@qq.com>
2025-01-06 20:25:52 +0800

    sequence table

---
+++ 2.coding/1.vector.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: 1.vector.cpp
+	> Author:YYF 
+	> Mail: 
+	> Created Time: Mon 06 Jan 2025 06:35:28 PM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
